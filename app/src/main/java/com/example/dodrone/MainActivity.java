320a8b8dcb8e94df428939ad510a4a7d503c1fdf
RbChoi <rayun.rb.choi@gmail.com>
2021-07-22 14:18:59 +0900

    :sparkles: Add Camera permission

--- app/src/main/java/com/example/dodrone/MainActivity.java
+++ app/src/main/java/com/example/dodrone/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.dodrone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //button = (Button) findViewById(R.id.)
     }
+    
 }
