ff2412d4b30d9acc0e17ef11cd7be4653c2ccfca
wjddus0605 <wonjeongyoun0605@gmail.com>
2021-09-22 07:32:47 +0900

    ADD navigation drawer

--- app/src/main/java/com/example/dodrone/DoDroneActivity.java
+++ app/src/main/java/com/example/dodrone/DoDroneActivity.java
@@ -1,20 +1,34 @@
 package com.example.dodrone;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.example.dodrone.assemble.AssemblyMainActivity;
 import com.example.dodrone.controlling.CtrlMainActivity;
 import com.example.dodrone.train.TrainMainActivity;
+import com.google.android.material.navigation.NavigationView;
 
-public class DoDroneActivity extends AppCompatActivity {
+import org.jetbrains.annotations.NotNull;
 
-    Button assemBtn, trainBtn, ctrlBtn, myPage;
+public class DoDroneActivity extends AppCompatActivity{
+
+    Button assemBtn, trainBtn, ctrlBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +38,6 @@ public class DoDroneActivity extends AppCompatActivity {
         assemBtn = findViewById(R.id.assemBtn);
         trainBtn = findViewById(R.id.trainBtn);
         ctrlBtn = findViewById(R.id.ctrlBtn);
-        myPage = findViewById(R.id.myPage);
 
         ctrlBtn.setEnabled(false);
 
@@ -52,21 +65,37 @@ public class DoDroneActivity extends AppCompatActivity {
             }
         });
 
-        myPage.setOnClickListener(new View.OnClickListener() {
+
+
+        //navigation 설정
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent myPageIntent = new Intent(getApplicationContext(), MyPageActivity.class);
-                startActivity(myPageIntent);
+                drawerLayout.openDrawer(GravityCompat.END);
             }
         });
 
-
-
+        NavigationView navigationView = findViewById(R.id.navView);
+        navigationView.setItemIconTintList(null);
     }
 
-
-
-
-
+    public boolean menuOnclick(MenuItem item) {
+        int id= item.getItemId();
+        if (id==R.id.nav_home) {
+            Intent intent = new Intent(this, DoDroneActivity.class);
+            startActivity(intent);
+        }
+        if (id==R.id.nav_myPage) {
+            Intent intent = new Intent(this, MyPageActivity.class);
+            startActivity(intent);
+        }
+        if (id==R.id.nav_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
