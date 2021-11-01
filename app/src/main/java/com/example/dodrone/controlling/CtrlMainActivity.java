5de5f662ee71b44b3c72ed5d85bbfeb996f1ec80
RbChoi <rayun.rb.choi@gmail.com>
2021-11-01 18:04:30 +0900

    :construction: Add the libraries of tflite

--- app/src/main/java/com/example/dodrone/controlling/CtrlMainActivity.java
+++ app/src/main/java/com/example/dodrone/controlling/CtrlMainActivity.java
@@ -2,14 +2,32 @@ package com.example.dodrone.controlling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.os.ParcelFileDescriptor;
+import com.google.android.odml.image.MLImage;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.dodrone.R;
+
 //import org.tensorflow.lite.Converter;
+import org.tensorflow.lite.Interpreter;
+
 import org.tensorflow.lite.support.model.Model;
 import org.tensorflow.lite.gpu.CompatibilityList;
-import org.tensorflow.lite.gpu.GpuDelegate;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class CtrlMainActivity extends AppCompatActivity {
 
@@ -24,12 +42,12 @@ public class CtrlMainActivity extends AppCompatActivity {
     if(compatList.isDelegateSupportedOnThisDevice()){
         // if the device has a supported GPU, add the GPU delegate
         options = new Model.Options.Builder().setDevice(Model.Device.GPU).build();
-    } else {
+    } //else {
         // if the GPU is not supported, run on 4 threads
-        options = new Model.Options.Builder().setNumThreads(4).build();
-    }
+        //options = new Model.Options.Builder().setNumThreads(4).build();
+    //}
 
-    MyModel myModel = new MyModel.newInstance(context, options);
+    //MyModel myModel = new MyModel.newInstance(context, options);
 
     // Run inference per sample code
 
@@ -38,6 +56,8 @@ public class CtrlMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ctrl_main);
+
+
     }
 
 }
